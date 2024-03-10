69e887dbe3b3fb377d536e47808f03f02c3c20fc
Stefanie Leutenegger <133067224+StefanieLeu@users.noreply.github.com>
2024-03-10 11:55:00 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,6 @@ const Footer = () => {
         '>
             <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
                 <span className=''>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-                <div className='flex items-center lg:py-2'>
-                Built with the help of
-                &nbsp;<Link href="https://github.com/codebucks27/Next.js-Developer-Portfolio-Starter-Code" target={"_blank"} className='underline underline-offset-4'>CodeBucks</Link>
-                &nbsp;|&nbsp;<Link href="https://undraw.co" target={"_blank"} className='underline underline-offset-4'>unDraw</Link>
-                </div>
                 <span className='semi-bold text-md text-primary dark:text-primaryDark'>&#706;/&#707;</span>
             </Layout>
         </footer>
